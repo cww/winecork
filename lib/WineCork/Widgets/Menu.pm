8dfb30d4ec09e5f59a85bbb880bbd1425b9bbc47
Colin Wetherbee <cww@denterprises.org>
2010-06-30 23:23:22 -0500

    Added documentation.

--- lib/WineCork/Widgets/Menu.pm
+++ lib/WineCork/Widgets/Menu.pm
@@ -23,8 +23,31 @@ use constant MENU_ITEMS =>
     },
 ];
 
+=head1 NAME
+
+WineCork::Widgets::Menu
+
+=head1 SYNOPSIS
+
+    use WineCork::Widgets::Menu;
+
+    my $menu_obj = WineCork::Widgets::Menu->new();
+    my $menu = $menu_obj->build();
+    $vbox->add($menu->{widget});
+
+=cut
+
+=head1 METHODS
+
+=cut
+
 class WineCork::Widgets::Menu
 {
+=head2 build()
+
+Builds the main application menu and returns a menu object.
+
+=cut
     method build()
     {
         my $menu = Gtk2::SimpleMenu->new
@@ -35,5 +58,24 @@ class WineCork::Widgets::Menu
         return $menu;
     }
 }
+
+=head1 AUTHOR
+
+Colin Wetherbee <cww@cpan.org>
+
+=head1 COPYRIGHT
+
+Copyright (C) 2010, Colin Wetherbee
+
+=head1 LICENSE
+
+This module is free software.  You can redistribute it and/or
+modify it under the terms of the Artistic License 2.0.
+
+This program is distributed in the hope that it will be useful,
+but without any warranty; without even the implied warranty of
+merchantability or fitness for a particular purpose.
+
+=cut
 
 1;
